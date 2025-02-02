6754938cdc5472414de4f51d6b8d86f3a6330fb9
Craig Dennis <craigsdennis@gmail.com>
2025-02-01 23:25:48 -0800

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.json`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+const app = new Hono<{ Bindings: Env }>();
+
+app.post('/api/channels', async(c) => {
+	// TODO...make this create a live input
+	const whipUrl = "https://customer-vmoocz19hv6zb932.cloudflarestream.com/5bfd96a7323d3795bbf30afffb28a318kc27ade28e00b4e501c0a75c648d45580/webRTC/publish";
+	const whepUrl = "https://customer-vmoocz19hv6zb932.cloudflarestream.com/c27ade28e00b4e501c0a75c648d45580/webRTC/play"
+    return c.json({whipUrl, whepUrl});
+});
+
+export default app;
